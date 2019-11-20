af97cb462930808d6dd7d5474e9d3a6b4f67969e
Shobi <shobihap@stud.ntnu.no>
2019-11-20 20:05:40 +0100

    feature selection plot

--- feature_selection.py
+++ feature_selection.py
@@ -24,14 +24,13 @@ ch_fs_instances = []
 ch_tags_instances = []
 ins = []
 sub_ins_m = []
-for subject in range(1, 11):  # 26
+for subject in range(1, 27):  # 26
     s_s_chs = get_subdataset(subject, session)
     _index = [i + 1 for i, d in enumerate(s_s_chs[:, -1]) if d == 1]
     instances = get_samples(_index, s_s_chs, sr)
-    for f_instance in range(0, 15):
+    for f_instance in range(0, 60):
         instance = np.array(instances[f_instance, :, 1:-1]).transpose()
-        ins14 = instance[[4, 5, 7, 9, 13, 15, 17, 23, 25, 33, 43, 51, 55, 56], :]
-        ch_fs_instances.append(get_features_emd(ins14, sr))
+        ch_fs_instances.append(get_features_emd(instance, sr))
     sub_ins_m.append(np.mean(ch_fs_instances, axis=0))
     ch_tags_instances.append('subject_{0}'.format(subject))
 dataset = {"data": sub_ins_m, "target": ch_tags_instances}
@@ -43,7 +42,8 @@ for i, ii in enumerate(dataset['data']):
     plt.plot(ii, linewidth=0.8, label='Subject {0}'.format(i))
 plt.xlabel('Feature')
 plt.ylabel('Value')
-plt.grid(True)
+#plt.grid(True)
 plt.legend()
-plt.show()
+plt.title('Statistical features')
+plt.savefig('features_stat.png')
 
