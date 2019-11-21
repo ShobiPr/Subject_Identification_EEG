de42d75086c5956274004a42ae8d3e1b679204ea
ShobiPr <shobihap@stud.ntnu.no>
2019-11-21 19:54:55 +0100

    feature selection for P300 added

--- feature_selection_P300.py
+++ feature_selection_P300.py
@@ -28,7 +28,7 @@ session = 1
 ch_fs_instances = []
 ch_tags_instances = []
 sub_ins_m = []
-for subject in range(1, 27):  # 26
+for subject in range(2, 27):  # 26
     s_s_chs = get_subdataset(subject, session)
     _index = [i + 1 for i, d in enumerate(s_s_chs[:, -1]) if d == 1]
     instances = get_samples(_index, s_s_chs, sr)
@@ -37,10 +37,9 @@ for subject in range(1, 27):  # 26
         ch_fs_instances.append(get_features_emd(instance, sr))
     sub_ins_m.append(np.mean(ch_fs_instances, axis=0))
     print("Subjects {0} done".format(subject))
-dataset = {"data": sub_ins_m, "target": ch_tags_instances}
 
 
-for i, ii in enumerate(dataset['data']):
+for i, ii in enumerate(sub_ins_m):
     plt.plot(ii, linewidth=0.8)
 plt.xlabel('Feature')
 plt.ylabel('Value')
