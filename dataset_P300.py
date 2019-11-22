b29f77412380b16330b8c8d2cfd4ca35db198df1
Shobi <shobihap@stud.ntnu.no>
2019-11-22 12:24:35 +0100

    notch filter added

---
+++ dataset_P300.py
@@ -0,0 +1,31 @@
+from PyEMD import EEMD, Visualisation
+import numpy as np
+import math
+
+
+def get_samples(_index, s_s_chs, sr, _size=1.3):
+    instances = []
+    for _ind in _index:
+        instances.append(s_s_chs[_ind:int(math.ceil(_ind + (_size * sr)))][:])
+    return np.array(instances)
+
+
+def get_subdataset(_S=1, Sess=1):
+    _file = 'P300/train/Data_S%02d_Sess%02d.csv' % (_S, Sess)
+    _f = open(_file).readlines()
+    channels = []
+    for i, _rows in enumerate(_f):
+        if i > 0:
+            channels.append(eval(_rows))
+        else:
+            _headers = _rows
+    return np.array(channels)
+
+
+def get_dataset():
+    sr = 200
+    s_s_chs, _header = get_subdataset()
+    _index = [i + 1 for i, d in enumerate(s_s_chs[:, -1]) if d == 1]
+    instances = get_samples(_index, s_s_chs, sr)  # (60, 260, 59)
+    instance = np.array(instances[1, :, 1:-1]).transpose()  # (57, 260)
+    return instance
---
+++ training_models/P300/EMD_P300_stat/EMD_P300_ch14_stat.py
@@ -0,0 +1,60 @@
+from dataset import get_subdataset, get_samples
+from features import get_imfs_emd, get_statistics_values
+from preprocessing import preprocessing
+from classefiers import random_forest, decision_tree, knn, SVM, naive_bayes, selector
+
+import pickle
+import logging
+import warnings
+
+warnings.filterwarnings("ignore")
+logging.basicConfig(filename='EMD_P300_ch14_stat.log',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s')
+
+
+def get_features_emd(instance):
+    features_vector = []
+    for i, channel in enumerate(instance):
+        imfs = get_imfs_emd(channel)
+        if len(imfs) > 1:
+            features_vector += get_statistics_values(imfs)
+    return features_vector
+
+def main():
+    logging.info(" ***** CHANNELS:14, FEATURES: stat ***** \n")
+    logging.info(" ---------- With preprocessing ---------- \n")
+    INSTANCES = [10, 20, 40, 60]
+    sr = 200
+    lowcut = 0.1
+    highcut = 70.0
+    order = 4
+    for ins in INSTANCES:
+        logging.info(" -------- Instance: {0} --------".format(ins))
+        ch_fs_instances = []
+        ch_tags_instances = []
+        for subject in range(1, 27):  # 26
+            for session in range(1, 5):  # 4
+                s_s_chs = get_subdataset(subject, session)
+                _index = [i + 1 for i, d in enumerate(s_s_chs[:, -1]) if d == 1]
+                instances = get_samples(_index, s_s_chs, sr)
+                for f_instance in range(0, ins):
+                    instance = preprocessing(lowcut, highcut, f_instance, instances, order, sr)
+                    ins14 = instance[[4, 5, 7, 9, 13, 15, 17, 23, 25, 33, 43, 51, 55, 56], :]
+                    ch_fs_instances.append(get_features_emd(ins14))  # CHANNELS:14
+                    ch_tags_instances.append('subject_{0}'.format(subject))
+        dataset = {"data": ch_fs_instances, "target": ch_tags_instances}
+
+        dataTraining = dataset['data']
+        targetTraining = dataset['target']
+        result = selector(dataTraining, targetTraining)
+
+        logging.info("Best classifier {0} with accuracy {1} \n".format(result['classifier'], result['accuracy']))
+
+        # saving the model
+        model_name = 'EMD_P300_ch14_stat_ins%02d.sav' % (ins)
+        pickle.dump(result["model"], open(model_name, 'wb'))
+
+
+if __name__ == '__main__':
+    main()
