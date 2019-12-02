8aea45445a4195ef709fb2ec7e8abb2307f0b6e4
Shobi <shobihap@stud.ntnu.no>
2019-12-02 09:28:12 +0100

    setup update

---
+++ feature_selection/feature_selection_resting_state.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import numpy as np
+import scipy.io as spio
+from features import get_features_emd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+
+
+def consecutive_index(data, _value, stepsize=1):
+    data = np.where(data == _value)[0]
+    result = np.split(data, np.where(np.diff(data) != stepsize)[0] + 1)
+    return [[min(a), max(a)] for a in result]
+
+
+def get_dataset():
+    # samples_subject = 21000 # samples per subject
+    samples_trial = 7000  # samples per trial
+    no_subjects = 8
+    no_ch = 14
+    sr = 128
+    ch_fs_instances = []
+    ch_tags_instances = []
+    sub_ins_m = []
+    instance_len = sr * 2  # to create sub instances of 2 sec
+    EEG_data = spio.loadmat('EID-M.mat', squeeze_me=True)['eeg_close_ubicomp_8sub'].T  # (15, 168000)
+    _labels = EEG_data[14, :]  # len(168000)
+    for subject in range(1, no_subjects + 1):
+        s_index = consecutive_index(EEG_data[no_ch, :], subject)[0]  # [0, 20999]
+        _instance = EEG_data[:no_ch, s_index[0]:s_index[0] + samples_trial]  # (14, 7000)
+        max_instances = _instance.shape[1] / instance_len  # this is not necessary, but I added it just FYI, 27
+        for _i in range(0, 20): #range(0, 20):  # sub instances
+            if _i < max_instances:
+                index_start, index_end = instance_len * _i, instance_len * (_i + 1)
+                sub_instance = _instance[:, index_start:index_end]
+                print('sub-instance {0} with shape {1} for subject {2}'.format(_i, sub_instance.shape, subject))
+
+            ch_fs_instances.append(get_features_emd(subject, sr))
+
+            #ch_fs_instances.append(get_features_emd(sub_instance, sr))
+    #return {"data": ch_fs_instances}
+    return None
+
+
+get_dataset()
+
+# data = get_dataset()
+
+
+"""
+for i, ii in enumerate(data['data']):
+    plt.plot(ii, linewidth=0.8)
+plt.xlabel('Feature')
+plt.ylabel('Value')
+plt.grid(True)
+plt.title('Statistical features')
+plt.savefig('features_stat.svg', format='svg')
+plt.show()
+"""
---
+++ minkowski/EMD_best_IMF.py
@@ -0,0 +1,33 @@
+from dataset_P300 import get_subdataset, get_samples
+from features import get_features_emd
+from preprocessing import preprocessing_P300
+
+import logging
+import warnings
+
+warnings.filterwarnings("ignore")
+logging.basicConfig(filename='minkowski.log',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s')
+
+
+def main():
+    logging.info(" ***** Minkowski distance ***** \n")
+    sr = 200
+
+    ch_fs_instances = []
+    ch_tags_instances = []
+    for subject in range(1, 2):  # 26
+        for session in range(1, 2):  # 4
+            s_s_chs = get_subdataset(subject, session)
+            _index = [i + 1 for i, d in enumerate(s_s_chs[:, -1]) if d == 1]
+            instances = get_samples(_index, s_s_chs, sr)
+            for f_instance in range(0, 60):
+                logging.info("Instance: {0}".format(f_instance + 1))
+                instance = preprocessing_P300(instances, f_instance, sr)
+                ch_fs_instances.append(get_features_emd(instance, sr))
+        print("subject {0}: ".format(subject))
+
+
+if __name__ == '__main__':
+    main()
